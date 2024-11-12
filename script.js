9b8f8831e0fd6acf91c3d1069560fc7ce8cf705a
sanacheval <sanachevall@gmail.com>
2024-11-12 13:16:57 +0500

    completed 16x16 grid sketchbook

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let container = document.querySelector("div.container");
 
+container.style.height = "50vw";
+container.style.width = "50vw";
+container.style.margin = "auto";
+container.style.padding = "auto";
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.border = "1px solid black";
+
 for (let i=0; i<16; i++) {
+    let row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.flexWrap = "wrap";
+    row.style.width = "50vw";
     for (let i=0; i<16; i++) {
-        let div = document.createElement("div");
-        div.style.border = "1px solid black";
-        div.style.margin = "auto";
-        div.style.padding = "2.9%";
+        let div = document.createElement(`div`);
+        //div.style.border = "1px solid black";
+        div.style.flex = "1";
         div.addEventListener("mouseover", () => {
-            div.style.background = "pink";
+            div.style.background = "black";
         })
-        container.appendChild(div);
+        row.appendChild(div);
     }
-    container.style.margin = "auto";
-    container.style.display = "flex";
-    container.style.flexWrap = "wrap";
-    container.style.border = "none";
+    container.appendChild(row);
 }
